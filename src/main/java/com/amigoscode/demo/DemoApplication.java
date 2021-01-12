8e78cfcd771baeabb3331f1b76a82d477c080e6c
Gam <trant48@student.douglascollege.ca>
2021-01-12 14:48:01 -0800

    Added notification model

--- src/main/java/com/amigoscode/demo/DemoApplication.java
+++ src/main/java/com/amigoscode/demo/DemoApplication.java
@@ -8,5 +8,11 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        printMessage();
+    }
+
+    private static void printMessage() {
+        String message = "Hello Gam, How are you?";
+        System.out.println(message);
     }
 }
